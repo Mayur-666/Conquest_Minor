45ba258320103452e64f10e6da50f44a2ce879bc
Himanshu4782652 <127956737+Himanshu4782652@users.noreply.github.com>
2024-04-25 04:57:45 +0530

    Update App.jsx
    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,49 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import store from "./store/store.js";
 import { loadUser } from "./actions/userActions.js";
+import React, {useState} from 'react';
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const isAuth = useSelector((state) => state.isAuth);
   useEffect(() => {
     store.dispatch(loadUser());
   });
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  }, [])
   return (
     <>
-      <Navbar isAuthUser={isAuth} />
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/home" element={<Dashboard />} />
-        <Route exact path="/interview" element={<InterviewPage />} />
-        <Route exact path="/category" element={<JobCategory />} />
-        <Route exact path="/report" element={<ReportPage />} />
-        <Route exact path="/register" element={<RegisterLogin />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/history" element={<History />} />
-      </Routes>
+      {
+        loading ?
+        <HashLoader
+            // color={blue}
+            loading={loading}
+            //color={blue}
+            // cssOverride={override}
+            size={50}
+            color={"#36d7b7"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            speedMultiplier={1}
+        />
+        :
+        <Navbar isAuthUser={isAuth} />
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/home" element={<Dashboard />} />
+          <Route exact path="/interview" element={<InterviewPage />} />
+          <Route exact path="/category" element={<JobCategory />} />
+          <Route exact path="/report" element={<ReportPage />} />
+          <Route exact path="/register" element={<RegisterLogin />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/history" element={<History />} />
+        </Routes>
+      }
     </>
   );
 }
